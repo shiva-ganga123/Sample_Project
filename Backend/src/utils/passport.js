752625dc45e7c96137b5e3facd61771b0189f4e2
Shiva kumar Ganga <shivakumarganga@Shivas-Air.attlocal.net>
2025-08-28 18:17:12 -0500

    modified passport file.

--- Backend/src/utils/passport.js
+++ Backend/src/utils/passport.js
@@ -1,7 +1,7 @@
-import passport from 'passport.js';
+import passport from 'passport';
 
 import {Strategy as GoogleStrategy} from 'passport-google-oauth20';
-import User from '../models/User';
+import User from '../models/User.js';
 
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
