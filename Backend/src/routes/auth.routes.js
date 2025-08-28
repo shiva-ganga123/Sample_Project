82c4a4cd73a16f4ab16c79812d7032c770f958d6
Shiva kumar Ganga <shivakumarganga@Shivas-Air.attlocal.net>
2025-08-28 17:10:32 -0500

    Added routes to the app.

---
+++ Backend/src/routes/auth.routes.js
@@ -0,0 +1,19 @@
+import express from 'express';
+
+import {register, login, refresh, logout} from '../controllers/auth.controller.js';
+import passport from 'passport';
+
+const router = express.Router();
+
+router.post('/register', register);
+router.post('/login', login);
+router.post('/refresh', refresh);
+router.post('/logout', logout);
+
+// Google OAuth
+router.get('/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
+router.get('/google/callback', passport.authenticate('google', { failureRedirect: '/login' }),
+  (req, res) => res.redirect(process.env.CLIENT_ORIGIN)
+);
+
+export default router;
