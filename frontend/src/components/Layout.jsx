63a9c5860184332ebb13cc0591a89bfe1a8d589d
Shiva kumar Ganga <shivakumarganga@Shivas-Air.attlocal.net>
2025-08-28 14:41:30 -0500

    Created layout and added navbar.

---
+++ frontend/src/components/Layout.jsx
@@ -0,0 +1,11 @@
+import {Container, Row, Col} from 'react-bootstrap';
+
+export default function Layout() {
+    return (
+        <Container className='min-vh-100 d-flex justify-content-center align-items-center'>
+            <Row className='w-100'>
+                <Col xs={12} md={{ span: 6, offset: 3 }} lg={{ span: 4, offset: 4 }}></Col>
+            </Row>
+        </Container>
+    )
+}
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import { Navbar, Nav, Container } from "react-bootstrap";
+
+export default function Navbar() {
+    return (
+        <Navbar bg='primary' variant='dark' expand='lg' className='mb-4'>
+            <Container>
+                <Navbar.Brand href='/'>Life Tracker</Navbar.Brand>
+                <Navbar.Toggle aria-controls='navbar-nav'/>
+                <Navbar.Collapse id='navbar-nav'>
+                    <Nav className='ms-auto'>
+                        <Nav.Link href='/Dashboard'>Dashboard</Nav.Link>
+                        <Nav.Link href='/Login'>Login</Nav.Link>
+                        <Nav.Link href='/Register'>Register</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    )
+}
